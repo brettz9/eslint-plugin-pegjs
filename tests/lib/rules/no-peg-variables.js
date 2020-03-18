9068a6f6e92c124ba3f5e33351b9e09e06897e89
Brett Zamir <brettz9@yahoo.com>
2020-03-18 20:23:26 +0800

    - npm: Update devDep. and `package-lock.json` - Linting

--- tests/lib/rules/no-peg-variables.js
+++ tests/lib/rules/no-peg-variables.js
@@ -2,36 +2,32 @@
  * @fileoverview Prevent use of the PEG.js variables used internally
  * @author Brett Zamir
  */
-"use strict";
+'use strict';
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/no-peg-variables"),
-
-    RuleTester = require("eslint").RuleTester;
+const rule = require('../../../lib/rules/no-peg-variables'),
+  {RuleTester} = require('eslint');
 
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
-ruleTester.run("no-peg-variables", rule, {
-
-    valid: [
-
-        // give me some code that won't trigger a warning
-    ],
-
-    invalid: [
-        {
-            code: "peg$parse = 5",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
-    ]
+const ruleTester = new RuleTester();
+ruleTester.run('no-peg-variables', rule, {
+  valid: [
+    // give me some code that won't trigger a warning
+  ],
+  invalid: [
+    {
+      code: 'peg$parse = 5',
+      errors: [{
+        message: 'Fill me in.',
+        type: 'Me too'
+      }]
+    }
+  ]
 });
